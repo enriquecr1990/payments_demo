cee5c48d6aea80517e6a1d50201b5c6ec67a9392
Enrique Corona <enrique_cr1990@hotmail.com>
2025-05-27 16:42:00 -0600

    actualizacion del broker bcp para purebas de credenciales productivas

--- bcp/private/Bcp.php
+++ bcp/private/Bcp.php
@@ -7,17 +7,31 @@ class Bcp{
      private $params;
 
      public function __construct(){
-          $this->params['url'] = 'https://sandbox.openbanking.bcp.com.bo/Web_ApiQr/api/v4/Qr/Generated';
+          // $this->params['url'] = 'https://sandbox.openbanking.bcp.com.bo/Web_ApiQr/api/v4/Qr/Generated';
+          // $this->params['username'] = 'OMNILIFE_USER';
+          // $this->params['password'] = 'hhgce88a0234d3b43232a85464aadfr';
+          // $this->params['user_id'] = 'OMNILIFEUser28032025';
+          // $this->params['public_token'] = 'C9F13996-C678-4164-B54E-3195426AFEC5';
+          // $this->params['service_code'] = '050'; //validar este campo con alan, probamos con el de la documentaicon
+          // $this->params['bussiness_code'] = '0341';
+          // // $this->params['certificate_pfx'] = 'D:\Desarrollo\personales\metodos_pago\bcp\private\BCP_SANDBOX_OPENBANKING.pfx';
+          // // $this->params['certificate_pem'] = 'D:\Desarrollo\personales\metodos_pago\bcp\private\bcpcertificate.pem';
+          // $this->params['certificate_pfx'] = __DIR__.'/BCP_SANDBOX_OPENBANKING.pfx';
+          // $this->params['certificate_pem'] = __DIR__.'/bcpcertificate.pem';
+          // // $this->params['certificate_pfx'] = 'D:\Desarrollo\personales\metodos_pago\bcp\private\bcpcertificate.pem';
+          // $this->params['password_pfx'] = 'Arquitectura2025$';
+          //datos para probar produccion
+          $this->params['url'] = 'https://openbanking.bcp.com.bo/Web_ApiQr/api/v4/Qr/Generated';
           $this->params['username'] = 'OMNILIFE_USER';
-          $this->params['password'] = 'hhgce88a0234d3b43232a85464aadfr';
-          $this->params['user_id'] = 'OMNILIFEUser28032025';
-          $this->params['public_token'] = 'C9F13996-C678-4164-B54E-3195426AFEC5';
+          $this->params['password'] = 'm,szYTq4xL97ga.v';
+          $this->params['user_id'] = 'OMNILIFEUser20250516';
+          $this->params['public_token'] = 'A2A8E689-CDA5-46C4-9B82-9431EDD76688';
           $this->params['service_code'] = '050'; //validar este campo con alan, probamos con el de la documentaicon
-          $this->params['bussiness_code'] = '0341';
-          $this->params['certificate_pfx'] = 'D:\Desarrollo\personales\metodos_pago\bcp\private\BCP_SANDBOX_OPENBANKING.pfx';
-          $this->params['certificate_pem'] = 'D:\Desarrollo\personales\metodos_pago\bcp\private\bcpcertificate.pem';
+          $this->params['bussiness_code'] = '0491';
+          $this->params['certificate_pfx'] = __DIR__.'/OMNIB.pfx';
+          $this->params['certificate_pem'] = __DIR__.'/OMNIB.pem';
           // $this->params['certificate_pfx'] = 'D:\Desarrollo\personales\metodos_pago\bcp\private\bcpcertificate.pem';
-          $this->params['password_pfx'] = 'Arquitectura2025$';
+          $this->params['password_pfx'] = 'yGi4.50kDqo,jUel';
      }
 
      public function b64UP(){
@@ -27,7 +41,7 @@ class Bcp{
      public function createQR(){
           try{
                $curl = curl_init();
-               $identificador = 'Omni-Test-'.date('YmdHis');
+               $identificador = 'Omni-TestPROD-'.date('YmdHis');
                $authorization = base64_encode($this->params['username'].':'.$this->params['password']);
                $headers = [
                     'Content-Type: application/json',
@@ -38,7 +52,7 @@ class Bcp{
                     'appUserId' => $this->params['user_id'],
                     'currency' => 'BOB', //moneda de bolivia
                     'amount' => 150.55, //formato numerico con dos decimales
-                    'gloss' => '', //generar qr como campo dinamico
+                    'gloss' => $identificador, //generar qr como campo dinamico
                     'enableBank' => 'ALL', //codigo de servicio asignado a la empresa
                     'serviceCode' => $this->params['service_code'], //codigo de servicio asignado a la empresa
                     'businessCode' => $this->params['bussiness_code'], //codigo de empresa asignado
@@ -66,7 +80,7 @@ class Bcp{
                     CURLOPT_SSL_VERIFYHOST => '2',
                     CURLOPT_SSL_VERIFYPEER => false,
                     CURLOPT_SSLCERT =>  $this->params['certificate_pem'],
-                    CURLOPT_SSLCERTPASSWD => 'Arquitectura2025$',
+                    CURLOPT_SSLCERTPASSWD => $this->params['password_pfx'],
                );
 
                $return = ComunHelper::curlopt($this->params['url'],$options);
@@ -160,14 +174,14 @@ class Bcp{
           try{
                //validamos si no existe el archivo PEM, para procesarlo para el curlopt
                if(!file_exists($this->params['certificate_pem'])){
-                    if (openssl_pkcs12_read(file_get_contents('D:\Desarrollo\personales\metodos_pago\bcp\private\BCP_SANDBOX_OPENBANKING.pfx'), $certs, 'Arquitectura2025$')) {
+                    if (openssl_pkcs12_read(file_get_contents($this->params['certificate_pfx']), $certs, $this->params['password_pfx'])) {
                          file_put_contents($this->params['certificate_pem'],$certs['cert'].$certs['pkey']);
                          // echo 'se convirtio pfx correctamente<br>';
                     }else{
                          // echo 'ocurrio un error';exit;
                     }
                }else{
-                    // echo 'ya existia el pem desde pfx';
+                    // echo 'ya  existia el pem desde pfx';
                }
           }catch(Exception $ex){
                // echo 'ocurrio un error al transformar el PFX a PEM';
